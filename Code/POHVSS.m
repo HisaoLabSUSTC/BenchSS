db5fb46ce1f3b9ef3a5b57af0b322265e0affc27
Tianye Shu <47651611+SliverySky@users.noreply.github.com>
2022-06-20 15:43:06 +0800

    update

---
+++ Code/POHVSS.m
@@ -0,0 +1,57 @@
+function [selVal,time] = POHVSS(objVal,selNum,ref)
+    tic;
+    [length,~] = size(objVal);
+    ref = max(objVal,[],1)*ref;
+    s = zeros(1,length);
+    Population = s;
+    Obj = [0,Inf];
+    
+    maxIteration = 1000;
+    currentIteration = 0;   
+    
+    while currentIteration < maxIteration
+        [N,~] = size(Population);
+        index = randperm(N,1);
+        offspring = Population(index,:);
+        offspring = mutation(offspring,length);
+        if sum(offspring)==0
+           continue 
+        end
+        Population = [Population;offspring];
+        objs = calObj(offspring,objVal,selNum,ref);
+        Obj = [Obj;objs];
+        
+        [FrontNo,~] = NDSort(Obj,1);
+        Population = Population(FrontNo==1,:);
+        Obj = Obj(FrontNo==1,:);
+        currentIteration = currentIteration+1;
+        if toc > 3600
+            break
+        end
+    end
+    
+    check = Obj(:,1)>selNum;
+    Obj(check,:) = [];
+    Population(check,:) = [];
+    
+    [~,index] = min(Obj(:,2));
+    minoffspring = Population(index,:);
+    selVal = objVal(minoffspring==1,:);
+    time = toc;
+end
+
+function offspring = mutation(offspring,length)
+    Site = rand(1,length) < 1/length;
+    offspring(Site) = ~offspring(Site);
+end
+
+function objs = calObj(offspring,objVal,selNum,ref)
+    obj1 = sum(offspring);
+    if obj1 >= 2*selNum
+        obj2 = Inf;
+    else
+        selected = objVal(offspring==1,:);
+        obj2 = -stk_dominatedhv(selected,ref);
+    end
+    objs = [obj1,obj2];
+end
